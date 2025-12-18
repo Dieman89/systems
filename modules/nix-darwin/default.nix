8aa3e88bafa6cbdfa13e7c9e2b0a242d39522109
Dieman89 <alessandro.buonerba@kaluza.com>
2025-12-18 18:15:45 +0000

    Add macOS system settings from current machine

--- modules/nix-darwin/default.nix
+++ modules/nix-darwin/default.nix
@@ -22,33 +22,54 @@
   system.defaults = {
     dock = {
       autohide = true;
-      mru-spaces = false;
+      autohide-time-modifier = 0.0;
+      tilesize = 54;
+      largesize = 16;
+      magnification = false;
+      orientation = "bottom";
       minimize-to-application = true;
+      mineffect = "scale";
       show-recents = false;
+      mru-spaces = false;
+
+      # Hot corners
+      # Values: 0=none, 2=mission-control, 3=app-windows, 4=desktop,
+      #         5=screensaver, 6=disable-screensaver, 10=sleep-display,
+      #         11=launchpad, 12=notification-center, 13=lock-screen, 14=quick-note
+      wvous-tl-corner = 5;   # Top-left: Start Screen Saver
+      wvous-tr-corner = 12;  # Top-right: Notification Center
+      wvous-bl-corner = 11;  # Bottom-left: Launchpad
+      wvous-br-corner = 4;   # Bottom-right: Desktop
     };
 
     finder = {
       AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
       ShowPathbar = true;
       ShowStatusBar = true;
-      FXPreferredViewStyle = "clmv"; # Column view
-      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "icnv"; # Icon view
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowHardDrivesOnDesktop = false;
+      ShowRemovableMediaOnDesktop = true;
     };
 
     NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
       AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
-      InitialKeyRepeat = 15;
-      KeyRepeat = 2;
-      ApplePressAndHoldEnabled = false;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
+      AppleReduceDesktopTinting = true;
+      NSAutomaticCapitalizationEnabled = true;
+      NSAutomaticSpellingCorrectionEnabled = true;
+    };
+
+    menuExtraClock = {
+      ShowAMPM = true;
+      ShowDate = 0; # Don't show date
+      ShowDayOfWeek = true;
     };
 
     trackpad = {
-      Clicking = true;
-      TrackpadThreeFingerDrag = true;
+      Clicking = false;
+      TrackpadThreeFingerDrag = false;
+      TrackpadRightClick = true;
     };
   };
 
