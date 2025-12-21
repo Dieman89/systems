cc8b4d5acf502822bba52a0689f73f0cda463c2e
dieman <28837891+Dieman89@users.noreply.github.com>
2025-12-20 19:48:41 -0600

    github signature

--- modules/nix-darwin/security.nix
+++ modules/nix-darwin/security.nix
@@ -4,7 +4,7 @@ _:
   # Enable Touch ID for sudo
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  # Allow 1Password to connect to Zen browser (nix-wrapped)
+  # Allow 1Password to connect to Zen browser
   environment.etc."1password/custom_allowed_browsers" = {
     text = ''
       .zen-wrapped
